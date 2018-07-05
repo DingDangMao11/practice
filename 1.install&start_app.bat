3936b2922a089975624b75d4454c22f7e13bd1c3
wqk317 <wqk317226700@gmail.com>
2018-07-05 17:58:04 +0800

    update task.png

--- 1.install&start_app.bat
+++ 1.install&start_app.bat
@@ -1,2 +1,3 @@
+::adb shell dumpsys window windows | findstr "Current" //获取当前窗口
 adb wait-for-device install -r mobileqq_android-7.2.5.apk
 adb shell am start com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
