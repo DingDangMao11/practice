4b2b43e89dd09e80f8da2f07300f73074caf3359
Hi Hydra <wqk317226700@gmail.com>
2018-07-16 12:37:04 +0800

    Update 1.install&start_app.bat

--- 1.install&start_app.bat
+++ 1.install&start_app.bat
@@ -1,3 +1,3 @@
-::adb shell dumpsys window windows | findstr "Current" //获取当前窗口
+::adb shell dumpsys window windows | findstr "Current" //鑾峰彇褰撳墠绐楀彛
 adb wait-for-device install -r mobileqq_android-7.2.5.apk
 adb shell am start com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
